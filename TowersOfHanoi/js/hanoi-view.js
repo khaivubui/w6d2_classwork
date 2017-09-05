33772f992c2dfea6779712a9460d04d71f756fd2
App Academy Student <student@appacademy.io>
2017-09-05 16:22:25 -0700

    Finish game

--- TowersOfHanoi/js/hanoi-view.js
+++ TowersOfHanoi/js/hanoi-view.js
@@ -13,6 +13,7 @@ class View {
     for (let stack = 0; stack < 3; stack++) {
       const $stack = $('<ul>');
       $stack.addClass('stack');
+      $stack.attr('data-stack', stack);
       for (let slot = 0; slot < 3; slot++) {
         const $li = $('<li>');
         $li.attr('data-stack', stack);
@@ -25,6 +26,7 @@ class View {
   }
 
   render() {
+    $('li').removeClass('disc-1').removeClass('disc-2').removeClass('disc-3');
     const towers = this.game.towers;
     for (let stack = 0; stack < 3; stack++) {
       if (towers[stack].length > 0) {
@@ -39,13 +41,24 @@ class View {
   clickTower() {
     let startTowerIdx;
     let endTowerIdx;
-    $('li').on('click', function () {
-      if (typeof startTowerIdx === undefined) {
+    const view = this;
+    $('ul').on('click', function () {
+      if (typeof startTowerIdx !== 'undefined') {
         endTowerIdx = $(this).attr('data-stack');
         endTowerIdx = parseInt(endTowerIdx);
+        view.game.move(startTowerIdx, endTowerIdx);
+        view.render();
+        startTowerIdx = undefined;
+        endTowerIdx = undefined;
+        $('ul').css('border-color', 'black');
+        if (view.game.isWon()) {
+          view.$dom.parent().append($('<h1>YOU WON</h1>'));
+          $('ul').off('click');
+        }
       } else {
         startTowerIdx = $(this).attr('data-stack');
         startTowerIdx = parseInt(startTowerIdx);
+        $(this).css('border-color', '#3498db');
       }
       console.log(startTowerIdx, endTowerIdx);
     });
